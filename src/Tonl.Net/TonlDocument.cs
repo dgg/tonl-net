544131915a32c7e275c52e37504bbe5f49fe9f01
Daniel González García <danielgonnet@gmail.com>
2026-03-22 09:01:26 +0100

    feat: :sparkles: scaffold high-level creation interface

--- src/Tonl.Net/TonlDocument.cs
+++ src/Tonl.Net/TonlDocument.cs
@@ -1,6 +1,30 @@
-﻿namespace Tonl.Net;
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace Tonl.Net;
 
 /// <summary>
-/// 
+/// Provides the high-level interface for working with TONL documents.
 /// </summary>
-public class TonlDocument;
+/// <param name="node">The underlying document.</param>
+public class TonlDocument(JsonNode? node)
+{
+	/// <summary>
+	/// Gets the underlying document associated with the TONL document.
+	/// </summary>
+	public JsonNode? Node => node;
+
+	/// <summary>
+	/// Converts the specified data to a TONL document.
+	/// </summary>
+	/// <param name="data">The data to convert.</param>
+	/// <param name="options">Options to control the conversion behavior.</param>
+	/// <typeparam name="T"></typeparam>
+	/// <returns>A representation of the data in as a TONL document.</returns>
+	public static TonlDocument FromData<T>(T data, JsonSerializerOptions? options = null)
+	{
+		JsonNode? node = JsonSerializer.SerializeToNode(data, options);
+		var doc = new TonlDocument(node);
+		return doc;
+	}
+}
--- tests/Tonl.Net.Tests/TonlDocumentTester.cs
+++ tests/Tonl.Net.Tests/TonlDocumentTester.cs
@@ -5,18 +5,18 @@ namespace Tonl.Net.Tests;
 [TestFixture]
 public class TonlDocumentTester
 {
-    [SetUp]
-    public void Setup() { }
+	[SetUp]
+	public void Setup() { }
 
-    [Test]
-    public void Success()
-    {
-	    Assert.Pass();
-    }
-    
-    [Test, Explicit]
-    public void Failure()
-    {
-	    Assert.Fail("this test always fails");
-    }
+	[Test]
+	public void Success()
+	{
+		Assert.Pass();
+	}
+
+	[Test, Explicit]
+	public void Failure()
+	{
+		Assert.Fail("this test always fails");
+	}
 }
