ab6de1a998c8484995b9b3e694759754da6d3292
Daniel González García <danielgonnet@gmail.com>
2026-03-29 17:40:19 +0200

    feat: :technologist: strongly typed version

--- src/Tonl.Net/TonlVersion.cs
+++ src/Tonl.Net/TonlVersion.cs
@@ -1,6 +1,35 @@
 namespace Tonl.Net;
 
-public class TonlVersion
+/// <summary>
+/// Represents a TONL document version.
+/// </summary>
+/// <param name="Major">The major version number.</param>
+/// <param name="Minor">The minor version number.</param>
+public readonly record struct TonlVersion(ushort Major, ushort Minor = 0)
 {
-	
+	/// <summary>
+	/// Converts the value of the current <see cref="TonlVersion"/> to its equivalent <see cref="String"/> representation.
+	/// </summary>
+	/// <returns>The <see cref="String"/> representation of the values of the major and minor components of the current <see cref="TonlVersion"/>.
+	/// Each component is separated by a period character ('.').</returns>
+	public override string ToString() => $"{Major:N0}.{Minor:N0}";
+
+	/// <summary>
+	/// Converts the value of the current <see cref="TonlVersion"/> to its equivalent TONL string representation.
+	/// </summary>
+	/// <returns>The TONL string representation to be rendered in the header.</returns>
+	public string ToTonl() => $"#version {this}";
+
+	/// <summary>
+	/// The default TONL version (1.0).
+	/// </summary>
+	public static readonly TonlVersion Default = new(1, 0);
+
+	/// <summary>
+	/// Creates a new TONL version from a <see cref="Version"/>.
+	/// </summary>
+	/// <param name="version">The version.</param>
+	/// <returns>The equivalent <see cref="TonlVersion"/> to <paramref name="version"/>.</returns>
+	public static TonlVersion FromVersion(Version version) =>
+		new(Convert.ToUInt16(version.Major), Convert.ToUInt16(version.Minor));
 }
--- tests/Tonl.Net.Tests/TonlVersionTester.cs
+++ tests/Tonl.Net.Tests/TonlVersionTester.cs
@@ -1,7 +1,63 @@
+using Subject = Tonl.Net.TonlVersion;
+
 namespace Tonl.Net.Tests;
 
 [TestFixture]
 public class TonlVersionTester
 {
+	#region construction
+
+	[Test]
+	public void Ctor_MajorMinor_PropsSet()
+	{
+		var subject = new Subject(1, 2);
+
+		Assert.That(subject.Major, Is.EqualTo(1));
+		Assert.That(subject.Minor, Is.EqualTo(2));
+	}
 	
+	[Test]
+	public void Ctor_Major_ZeroMinor()
+	{
+		var subject = new Subject(7);
+
+		Assert.That(subject.Major, Is.EqualTo(7));
+		Assert.That(subject.Minor, Is.Zero);
+	}
+
+	[Test]
+	public void Default_OnePointOh()
+	{
+		var onePointOh = new TonlVersion(1);
+		Assert.That(Subject.Default, Is.EqualTo(onePointOh));
+	}
+
+	[Test]
+	public void FromVersion_AllComponents_OnlyMajorMinor()
+	{
+		var version = new Version(1, 2, 3, 4);
+		
+		Assert.That(Subject.FromVersion(version), Is.EqualTo(new Subject(1, 2)));
+	}
+
+	#endregion
+	
+	#region representation
+
+	[Test]
+	public void ToString_MajorDotMinor()
+	{	
+		var subject = new Subject(7, 8);
+		Assert.That(subject.ToString(), Is.EqualTo("7.8"));
+	}
+	
+	[Test]
+	public void ToTonl_HeaderRepresentation()
+	{	
+		var subject = new Subject(7, 8);
+		string tonl = "#version 7.8";
+		Assert.That(subject.ToTonl(), Is.EqualTo(tonl));
+	}
+	
+	#endregion
 }
